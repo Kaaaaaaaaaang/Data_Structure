e8982c3c3b35e58dea915a1581b1a21b35e748f3
Sori Kang <56942589+Kaaaaaaaaaang@users.noreply.github.com>
2020-12-29 12:24:37 +0900

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -10,14 +10,17 @@
 #include <stdio.h>
 
 void BubbleSort(int a[], int n){
-  int i, j, temp;
-  for(i=1; i<n; i++){
+  int i=0, j, temp, flag=1;
+  while(flag==1){
+    flag = 0;
+    i++;
     for(j=0; j<n-i; j++){
-      if(a[j]>a[j+1]){
-        temp = a[j];
-        a[j] = a[j+1];
-        a[j+1] = temp;
-      }
+       if(a[j] > a[j+1]){
+         temp = a[j];
+         a[j] = a[j+1];
+         a[j+1] = temp;
+         flag = 1;
+       }
     }
   }
 }
